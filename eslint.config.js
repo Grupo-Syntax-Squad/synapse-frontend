458178aa61d5602c00f49ff337d0e57524b5dda3
Gabriel Reis <gabrieldreisilva4@gmail.com>
2025-08-28 00:56:23 -0300

    docs: #1 add license archive and configure ts and tsx file lint options

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,30 @@
 import js from "@eslint/js";
 import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    ignores: ["dist/", "commitlint.config.ts"],
+    ignores: ["dist/", "commitlint.config.ts", "node_modules/"],
   },
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
@@ -23,17 +38,33 @@ export default defineConfig([
         },
       },
     ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
-        sourceType: "module",
-      },
-    },
     rules: {
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      react: pluginReact,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      "react-refresh/only-export-components": "warn",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
     },
   },
 ]);
